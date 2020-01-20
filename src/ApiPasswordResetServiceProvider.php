27ff52b158937657b628c4503f52edc70e61b55a
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2020-01-19 21:57:31 -0300

    loading routes

--- src/ApiPasswordResetServiceProvider.php
+++ src/ApiPasswordResetServiceProvider.php
@@ -23,6 +23,7 @@ class ApiPasswordResetServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 }
