f05a4f26ae6c6600b1ee7afd108b5211aa7e5922
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2020-01-20 17:12:53 -0300

    added config file

---
+++ src/config/apiPasswordReset.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'route' => [
+        'prefix' => 'api/v1/auth/',
+        'uri' => 'password',
+        'middleware' => 'api'
+    ],
+    'tokenSize' => 60,
+    'emailNotificationLink' => 'some/link/',
+    'bcryptPassword' => true
+
+];
