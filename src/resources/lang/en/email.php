f5e9f0c05d27a89fa464858e183873df5c8fc4ee
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2020-02-09 18:45:34 -0300

    added translation files

---
+++ src/resources/lang/en/email.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'subject' => 'Password recovery request',
+    'greeting' => 'Hello :name,',
+    'intro' => 'You are receiving this email because we received a password reset request for your account.',
+    'buttonText'=>'Change Password',
+    'noFurtherActionRequired'=>'If you did not request a password reset, no further action is required.'
+
+];
