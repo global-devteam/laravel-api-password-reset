32e53fed0675f93891a34ffb9b7af816fb74f647
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2020-02-09 18:45:34 -0300

    added localization
    minor fixes
    
    removing json files
    
    loading translations on provider
    
    removing typo
    
    trying to fix the loadTranslationsFrom method
    
    making translation files publishable
    
    adding app_path()
    
    adding base_path()
    
    minor fixing
    
    minor fixing
    
    minor fixing
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff
    
    trying new stuff

---
+++ src/resources/lang/en/api-password-recovery.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'subject' => 'Password recovery request',
+    'greeting' => 'Hello :name,',
+    'intro' => 'You are receiving this email because we received a password recovery request for your account.',
+    'buttonText'=>'Change Password',
+    'outro'=>'If you did not request a password recovery, no further action is required.',
+    'regards'=>'Regards,'
+
+];
