57e798b4671c03bddeb30d59ba329bcbfa3dc7ef
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2020-02-11 20:02:45 -0300

    trying new stuff

--- src/resources/views/notifications/api-password-recovery.blade.php
+++ src/resources/views/notifications/api-password-recovery.blade.php
@@ -1,15 +1,15 @@
 @component('mail::message')
     {{__('api-password-recovery.greeting',['name'=>$name])}}
 
-    {{__('api-password-recovery.intro')}}
+{{__('api-password-recovery.intro')}}
 
 @component('mail::button', ['url' => $url])
 {{__('api-password-recovery.buttonText')}}
 @endcomponent
 
-    {{__('api-password-recovery.outro')}}
+{{__('api-password-recovery.outro')}}
 
-    {{__('api-password-recovery.regards')}}
+{{__('api-password-recovery.regards')}}
 
-    {{ config('app.name') }}
+{{ config('app.name') }}
 @endcomponent
