eb0ab578a271c11549e71ddaf457a3643fc694c5
Beau Raines <beau.raines@gmail.com>
2020-04-20 13:51:45 -0700

    New polish translation file
    Includes translations for #31

---
+++ src/resources/lang/pl/api-password-recovery.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'subject'   => 'Żądanie odzyskania hasła',
+  'subject_success' => 'Hasło poprawnie zmienione',
+  'greeting'  => 'Dzień dobry :name,',
+  'intro'     => 'Otrzymujesz ten mail bo my otrzymaliśmy prośbę na odzyskanie hasła.',
+  'buttonText' => 'Zmień hasło',
+  'outro'     => 'Jeśli nie zażądałeś odzyskania hasła, dalsze czynności nie są wymagane.',
+  'regards'   => 'Pozdrowienia,',
+  'success_message' => 'Pomyślnie zmieniłeś hasło. Jeśli zmieniłeś hasło, nie musisz nic robić. Jeśli nie zmieniłeś hasła, chroń swoje konto.',
+
+];
